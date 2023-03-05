d122b4443c25f2e78442f66260f520bd0f8de017
Sergey Kuznetsov <kuuuzya@ya.ru>
2023-03-05 21:39:21 +0400

    Добавлен вывод введённых значений

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ st.write(x, 'sqr3', x**3)
 ap_hi = st.slider('Систолическое (верхнее) давление', 80, 150 )
 ap_lo = st.slider('Диастолическое (нижнее) давление', 40, 100 )
 
-st.checkbox("Курите?", key="smoke")
+st.checkbox("Курю", key="smoke")
+
+st.selectbox("Уровень холестерина",[0,1,2], key="cholesterol")
+
+st.write('Давление:', ap_hi,'/',ap_lo)
+st.write('Курение:', smoke)
+st.write('Уровень холестерина:', cholesterol)
