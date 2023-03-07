5d8e372d825a3b0fdea775cee66aee59c5024a3e
Sergey Kuznetsov <kuuuzya@ya.ru>
2023-03-07 20:55:24 +0400

    Added imt and error preprocessing

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ elif ap_hi < ap_lo:
 else:
     fl_ap = 1
 
-imt = weight / ((height / 100) ** 2)
-st.write('Жопа', imt.round(2))
+imt = round(weight / ((height / 100) ** 2),2)
+st.write('Жопа', imt)
 
 """ if (fl_ap == 1) and (fl_imt == 1):
     #output
